5e5a3ed8823f88fcff7020075c4eda1896d9199c
sb-2016 <swarnendu.banerjee2017@gmail.com>
2024-03-23 19:22:29 +0530

    Create area_of_shapes.py

---
+++ module_1/area_of_shapes.py
@@ -0,0 +1,9 @@
+# Calculate area of a rectangle function
+def area_of_rectangle(length, width):
+  area = length * width
+  print(f'Area of rectangle is {area}')
+
+# Calculate area of a circle function
+def area_of_circle(radius):
+  area = 3.14 * radius * radius
+  print(f'Area of circle is {area}')
